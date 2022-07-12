917fdd4cf5c52412d8d2fcac99bcb5f5d5964155
thuanVoxTiki <thuan.vo1@tiki.vn>
2022-07-12 17:28:08 +0700

    create account api

--- main.go
+++ main.go
@@ -1,18 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thuanVoxTiki/simple_bank/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
+	dsn := "postgresql://postgres:123456@localhost:5432/simple_bank"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db = db.Debug()
+	runService(db)
+	if err != nil {
+		fmt.Println("DB has Error")
+		panic(gorm.ErrInvalidDB)
+	}
 	fmt.Println("Hello world")
-	fmt.Println(sum(5))
 }
 
-func sum(num int) int {
-	sum := 0
-	for i := 0; i <= num; i++ {
-		sum += i
-	}
-	fmt.Printf("%T", num)
-	fmt.Println()
-	return sum
+func runService(db *gorm.DB) {
+	r := gin.Default()
+	r.GET("ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.POST("createAccount", func(ctx *gin.Context) {
+		var account models.Account
+		if err := ctx.ShouldBind(&account); err != nil {
+			fmt.Printf("Account %v", account)
+			panic(err)
+		}
+		account.Create_at = time.Now()
+		// var account = models.Account{
+		// 		id: 1,
+		// 		owner: "Phuc",
+		// 		balance: 100000.0,
+		// 		currency: "hihi",
+		// }
+		fmt.Printf("Account %v", account)
+
+		if err := db.Create(&account).Error; err != nil {
+			fmt.Printf("Error: %v", err)
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"message": err,
+			})
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Okila",
+		})
+
+	})
+	r.Run()
 }
